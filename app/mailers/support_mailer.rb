8109c230a6d1e312dccbada02716e850815d2004
Joshua Sierles <joshua@diluvia.net>
2015-06-09 00:48:00 +0200

    use info@rnplay.org as source address

--- app/mailers/support_mailer.rb
+++ app/mailers/support_mailer.rb
@@ -1,5 +1,5 @@
 class SupportMailer < ApplicationMailer
   def contact(email, message)
-    mail(to: 'info@rnplay.org', subject: 'Support request', body: message)
+    mail(to: 'info@rnplay.org', from: 'info@rnplay.org', subject: 'Support request', body: message)
   end
 end
