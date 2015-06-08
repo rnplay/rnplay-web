5be9e2b4f568e77cd7a44dc624f71cb99d5c73a1
Joshua Sierles <joshua@diluvia.net>
2015-06-09 00:45:36 +0200

    use postmark key from environment, invalidate old key

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ module Rnplay
   class Application < Rails::Application
     config.active_record.raise_in_transactional_callbacks = true
     config.action_mailer.delivery_method = :postmark
-    config.action_mailer.postmark_settings = { :api_token => "3997a4ee-7134-4bb8-b148-a5c1c74c7859" }
+    config.action_mailer.postmark_settings = { :api_token => ENV['POSTMARK_KEY'] }
     config.active_job.queue_adapter = :sidekiq
     config.react.addons = true
 
