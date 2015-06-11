3817df58f3722737b7fd9480123c6511ef3d3c10
Joshua Sierles <joshua@diluvia.net>
2015-06-11 11:47:42 +0200

    only force ssl in prod/staging

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,14 +5,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session,
     if: Proc.new { |c| c.request.format == 'application/json' }
 
-  force_ssl if: :ssl_configured?
   rescue_from ActiveRecord::RecordNotFound, with: :not_found
 
   protected
-
-  def ssl_configured?
-   !Rails.env.development?
-  end
 
   def not_found
     redirect_to root_url
