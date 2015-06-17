6d8e7b5a094d1b7987f5ffdc0f600a62620e6327
Johannes Lumpe <johannes@johanneslumpe.de>
2015-06-18 00:08:16 +0300

    Initial commit

--- app/assets/javascripts/components/editor.js.jsx
+++ app/assets/javascripts/components/editor.js.jsx
@@ -1,8 +1,34 @@
+var makeFileTree = function (filenames) {
+  return filenames.reduce(function (tree, filename) {
+    var parts = filename.split('/');
+    var tempTree = tree;
+    var part;
+    while((part = parts.shift())) {
+      tempTree = tempTree[part] = parts.length === 0 ?
+        null :
+        tempTree[part] || {};
+    }
+    return tree;
+  }, {});
+};
+
 var Editor = React.createClass({
   getInitialState: function() {
+    var files = {
+      'index.js': this.props.app.body,
+      'folder/second.js': this.props.app.body.substring(0, 200),
+      'folder/nested/more.js': this.props.app.body.substring(100, 200),
+      'folder/different/third.js': this.props.app.body.substring(50, 400)
+    };
     return {
-      codeMirrorInstance: null
-    }
+      codeMirrorInstance: null,
+      unsavedBuffers: {},
+
+      // TODO remove this, as soon as we have real files passed in,
+      // just for testing purposes
+      files: files,
+      fileTree: makeFileTree(Object.keys(files))
+    };
   },
 
   _onChange: function(e) {
@@ -21,7 +47,7 @@ var Editor = React.createClass({
       indentUnit: 2,
       tabSize: 2,
       indentWithTabs: false
-    }
+    };
 
     if (this.props.useVimKeyBindings) {
       options.keyMap = 'vim';
@@ -39,16 +65,27 @@ var Editor = React.createClass({
     }
   },
 
+  changeFile: function (filename) {
+    this.state.codeMirrorInstance.setValue(this.state.files[filename]);
+    this.setState({
+      currentFile: filename
+    });
+  },
+
   render: function() {
+    var currentFile = this.state.currentFile || 'index.js';
     content = this.props.app.multiFile ?
     <div className="editor-flex-wrapper">
       <p>This app can't be edited yet, because it contains multiple files. Support for this coming soon!</p>
     </div>
     :
     <div className="editor-flex-wrapper">
-      <textarea ref="editorTextArea" onChange={this._onChange}>
-        {this.props.app.body}
-      </textarea>
+      <FileSelector files={this.state.fileTree} current={currentFile} onSelect={this.changeFile} />
+      <div className="editor-scroll-wrapper">
+        <textarea ref="editorTextArea" onChange={this._onChange}>
+          {this.props.app.body}
+        </textarea>
+      </div>
     </div>
 
     return content;
---
+++ app/assets/javascripts/components/fileselector.js.jsx
@@ -0,0 +1,106 @@
+var FileSelectorNode = React.createClass({
+
+  getInitialState: function () {
+    return {
+      open: false
+    };
+  },
+
+  onNodeClick: function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+
+    if (this.props.subtree) {
+      this.setState({
+        open: !this.state.open
+      });
+    } else {
+      this.props.onSelect(this.props.pathPrefix + this.props.label);
+    }
+  },
+
+  render() {
+    var subtree = this.props.subtree;
+    var onSelect = this.props.onSelect;
+    var label = this.props.label;
+    var current = this.props.current;
+    var pathPrefix = this.props.pathPrefix;
+    var fullPath = pathPrefix + label + (subtree ? '/' : '');
+
+    var sub = this.state.open && subtree && Object.keys(subtree)
+      .map(function (filenameOrPathSegment) {
+        return (
+          <FileSelectorNode
+            onSelect={onSelect}
+            current={current}
+            key={filenameOrPathSegment}
+            pathPrefix={fullPath}
+            label={filenameOrPathSegment}
+            subtree={subtree[filenameOrPathSegment]}
+          />
+        );
+      });
+
+    return (
+      <li
+        className={classNames({
+          'fileselector__list__node': true,
+          'fileselector__list__node--has-children' : !!subtree,
+          'fileselector__list__node--current': fullPath === current,
+          'fileselector__list__node--open': this.state.open
+        })}
+        onClick={this.onNodeClick}
+      >
+        <span className='fileselector__list__node__label'>{label}</span>
+        {this.state.open && sub && <ol className='fileselector__list'>{sub}</ol>}
+      </li>
+    );
+  }
+});
+
+
+var FileSelector = React.createClass({
+
+  getInitialState: function() {
+    return {
+      open: false
+    };
+  },
+
+  toggleSelector: function (e) {
+    e.preventDefault();
+    this.setState({
+      open: !this.state.open
+    });
+  },
+
+  render: function() {
+    var files = this.props.files;
+    var onSelect = this.props.onSelect;
+    var current = this.props.current;
+
+    return (
+      <div className={classNames({
+          fileselector: true,
+          open: this.state.open
+        })}>
+        <div className='open-handle' onClick={this.toggleSelector}></div>
+        <ol className='fileselector__list'>
+          {files && Object.keys(files).map(function (filename) {
+            return (
+              <FileSelectorNode
+                onSelect={onSelect}
+                current={current}
+                key={filename}
+                pathPrefix=''
+                label={filename}
+                subtree={files[filename]}
+              />
+            );
+          })}
+        </ol>
+      </div>
+    );
+  }
+
+});
