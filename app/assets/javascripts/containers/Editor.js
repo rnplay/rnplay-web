8688eb61ab37310901af67ee34e4a453cf64b370
Johannes Lumpe <johannes@johanneslumpe.de>
2015-06-23 01:06:18 +0300

    removed log

--- app/assets/javascripts/containers/Editor.js
+++ app/assets/javascripts/containers/Editor.js
@@ -6,20 +6,17 @@ import { connect } from 'redux/react';
 import { editor as editorActions } from '../actions';
 import EditorApp from '../components/EditorApp';
 
-@connect((state) => {
-  console.log(state.editor);
-  return {
-    app: state.editor.app,
-    name: state.editor.newName || state.editor.app.name,
-    buildId: state.editor.newBuildId || state.editor.app.buildId,
-    buildUpdated: !!state.editor.newBuildId,
-    currentFile: state.editor.currentFile,
-    fileTree: state.editor.fileTree,
-    fileBodies: state.editor.fileBodies,
-    showHeader: state.editor.showHeader,
-    appSaveInProgress: state.editor.appSaveInProgress
-  }
-})
+@connect((state) => ({
+  app: state.editor.app,
+  name: state.editor.newName || state.editor.app.name,
+  buildId: state.editor.newBuildId || state.editor.app.buildId,
+  buildUpdated: !!state.editor.newBuildId,
+  currentFile: state.editor.currentFile,
+  fileTree: state.editor.fileTree,
+  fileBodies: state.editor.fileBodies,
+  showHeader: state.editor.showHeader,
+  appSaveInProgress: state.editor.appSaveInProgress
+}))
 export default class Editor {
 
   render() {
