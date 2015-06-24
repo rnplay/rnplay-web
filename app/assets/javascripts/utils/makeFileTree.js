88758496839be9e800a687f47f45c45d968e1ab3
Johannes Lumpe <johannes@johanneslumpe.de>
2015-06-24 17:51:12 +0300

    Filetree is sorted now, has folder icons

--- app/assets/javascripts/utils/makeFileTree.js
+++ app/assets/javascripts/utils/makeFileTree.js
@@ -1,19 +1,29 @@
+'use strict';
 
+import natSort from './natSort';
 /**
  * Builds a nested object from an array of filepaths
  * @param  {array} filenames The filenames
  * @return {object}          The tree
  */
-export default (filenames) => {
-  return filenames.reduce((tree, filename) => {
-    const parts = filename.split('/');
-    let tempTree = tree;
-    let part;
-    while((part = parts.shift())) {
-      tempTree = tempTree[part] = parts.length === 0 ?
-        null :
-        tempTree[part] || {};
-    }
-    return tree;
-  }, {});
-};
+ export default (filenames) => {
+   // TODO we could also use _.difference here,
+   // but if this code does not slow us down then nevermind
+   const nested = filenames.filter((p) => p.indexOf('/') > -1).sort(natSort);
+   const files = filenames.filter((p) => p.indexOf('/') < 0).sort(natSort);
+
+   return files.reduce((tree, filename) => {
+     tree[filename] = null;
+     return tree;
+   }, nested.reduce((tree, filename) => {
+     const parts = filename.split('/');
+     let tempTree = tree;
+     let part;
+     while((part = parts.shift())) {
+       tempTree = tempTree[part] = parts.length === 0 ?
+         null :
+         tempTree[part] || {};
+     }
+     return tree;
+   }, {}));
+ };
