2da38cc8fc876327d1ee0789be0d7fc847fa196a
Joshua Sierles <joshua@diluvia.net>
2015-06-29 19:08:54 +0200

    only show clone link if the app belongs to the current user

--- app/assets/javascripts/components/EditorHeader.js
+++ app/assets/javascripts/components/EditorHeader.js
@@ -78,6 +78,7 @@ export default class EditorHeader extends Component {
   }
 
   renderGitButton() {
+    if (this.belongsToCurrentUser()) {
       return (
         <button
           onClick={this.showGitModal}
@@ -86,6 +87,7 @@ export default class EditorHeader extends Component {
           Clone
         </button>
       );
+    }
   }
 
   renderForkButton() {
