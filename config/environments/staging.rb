c0af03c2f3418712fcee7145decf617dd52e9193
Joshua Sierles <joshua@diluvia.net>
2015-06-29 17:35:14 +0200

    set default host for password reset

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -14,6 +14,8 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  config.action_mailer.default_url_options = { host: "staging.rnplay.org" }
+
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like
