9f0969537cabedbd6020472eecef94133367b74a
React Native Playground <info@rnplay.org>
2015-08-18 13:29:49 +0200

    fix bundle job

--- app/jobs/bundle_job.rb
+++ app/jobs/bundle_job.rb
@@ -1,10 +1,10 @@
 class BundleJob
-  include SuckerPunch::Job
+
+  include Sidekiq::Worker
 
   def perform(id)
-    ActiveRecord::Base.connection_pool.with_connection do
-      app = App.find(id)
-      app.bundle_js
-    end
+    app = App.find(id)
+    app.bundle_js
   end
+
 end
