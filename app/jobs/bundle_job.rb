129a3bcf1cacbdf0f3b2bba9e7f4546d26153148
React Native Playground <info@rnplay.org>
2015-08-18 03:29:43 +0200

    attempt sidekiq fix

--- app/jobs/bundle_job.rb
+++ app/jobs/bundle_job.rb
@@ -3,10 +3,8 @@ class BundleJob
   include Sidekiq::Worker
 
   def perform(id)
-    ActiveRecord::Base.connection_pool.with_connection do
-      app = App.find(id)
-      app.bundle_js
-    end
+    app = App.find(id)
+    app.bundle_js
   end
 
 end
