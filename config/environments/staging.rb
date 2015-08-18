f68d02fa06cfd95e707448c68283e3ea31126752
React Native Playground <info@rnplay.org>
2015-08-18 02:26:50 +0200

    log debug in staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -83,7 +83,6 @@ Rails.application.configure do
   config.logger = Logger.new(STDOUT)
   config.logger.level = Logger.const_get(log_level)
   config.log_level = log_level
-  config.lograge.enabled = true
 
 
 
