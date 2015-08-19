5daeca9a3d67dff0443bfe7a5d519b0a213f36a2
React Native Playground <info@rnplay.org>
2015-08-19 13:38:54 +0200

    make homepage work in staging and dev

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,13 @@
 class WelcomeController < ApplicationController
 
  def index
-   @app = Rails.env.development? ? App.first : App.find(7)
+   if Rails.env.development?
+     @app = App.create
+   elsif Rails.env.staging?
+     @app = App.first
+   else
+     @app = App.find(7) # Noodle soup app
+   end
  end
+
 end
