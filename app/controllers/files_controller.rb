8e7079a2bf2029f683383102f6f03ec282ad7057
Joshua Sierles <joshua@diluvia.net>
2015-08-21 14:44:34 +0200

    don't queue bundles yet in production

--- app/controllers/files_controller.rb
+++ app/controllers/files_controller.rb
@@ -11,7 +11,7 @@ class FilesController < ApplicationController
     @app = current_user.apps.find(params[:app_id])
     if @app
       @app.target_git_repo.update_file(params[:id], file_params[:body])
-      @app.queue_for_bundling
+      #@app.queue_for_bundling
       @app.target_git_repo.commit_all_changes("Updated from rnplay.org")
       render json: {message: "File updated."}, status: :ok
     else
