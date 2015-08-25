f6f1f166691cf9e08daf4dbd3c331dcbe00dec1a
React Native Playground <info@rnplay.org>
2015-08-25 17:53:19 +0200

    bump to rails 4.2.4

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,8 @@
 class WelcomeController < ApplicationController
-
  def index
    @app = Rails.env.development? || Rails.env.staging? ? App.first : App.find(7)
      @app = App.find(7) # Noodle soup app
    else
      @app = App.first
    end
- end
 end
