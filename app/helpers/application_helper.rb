af72d0d5ad9f4f2d6e0fc52d1cec2f99325445cb
Joshua Sierles <joshua@diluvia.net>
2015-09-06 22:47:51 +0200

    fix webpack include

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def fixed_webpack_include_tag(entry)
+    if WebpackRails.config.use_dev_server
+      javascript_include_tag("/webpack/dev-server/#{entry}")
+    else
+      javascript_include_tag("/webpack/#{WebpackRails::Precompiled[entry]['js']}")
+    end
+  end
+
 end
