6a6315a91cf90971ca02df8a59b8dbe1c45a9e74
Johan Ruokangas <johan@latehours.net>
2015-09-09 21:05:26 +0300

    Reorder and simplify QR modal's content

--- app/assets/javascripts/components/qr_modal.js
+++ app/assets/javascripts/components/qr_modal.js
@@ -27,12 +27,9 @@ export default class QrModal extends Component {
 
   renderFooter() {
     return (
-      <div className="modal__footer">
-        <img className="modal__footer__logo" src="/img/react-native-playground-logo.png" width="130" height="130" />
-        <div className="modal__footer--right">
-          <p>Run, test and share React Native applications directly on an iOS device.</p>
+      <div className="modal__header">
+          <p>Scan this with our <a href="https://itunes.apple.com/us/app/react-native-playground/id1002032944">iOS app</a>!</p>
           <img src="/img/app-store-badge.svg" width="135" height="40" />
-        </div>
       </div>
     );
   }
@@ -40,13 +37,10 @@ export default class QrModal extends Component {
   render() {
     return (
       <Modal isOpen={this.props.isOpen} onClickBackdrop={this.props.onClickBackdrop}>
+        {this.renderFooter()}
         <div className="modal__body qr-modal-body">
-          <h1>
-            Scan this with the <a href="https://itunes.apple.com/us/app/react-native-playground/id1002032944">React Native Playground iOS app</a>.
-          </h1>
           {this.renderQRImage()}
         </div>
-        {this.renderFooter()}
       </Modal>
     )
   }
