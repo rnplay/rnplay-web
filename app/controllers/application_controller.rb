472d5e86a79e936975cdc7904da3a752668f4547
Joshua Sierles <joshua@diluvia.net>
2015-09-10 19:51:10 -0500

    remove 404 redirect until we have a better 404

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,4 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session,
     if: Proc.new { |c| c.request.format == 'application/json' }
 
-  rescue_from ActiveRecord::RecordNotFound, with: :not_found
-
-  protected
-
-  def not_found
-    redirect_to root_url
-  end
 end
