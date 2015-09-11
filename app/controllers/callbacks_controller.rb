0ad9a7e17e23f78974393536d3fee9feb9441215
Joshua Sierles <joshua@diluvia.net>
2015-09-10 19:48:14 -0500

    switch to github authentication. also, if a user with the same email as in github had added an email previously, his apps are linked to the new account.

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -3,4 +3,12 @@ class CallbacksController < Devise::OmniauthCallbacksController
     @user = User.from_omniauth(request.env["omniauth.auth"])
     sign_in_and_redirect @user
   end
+
+  def github
+    @user = User.from_omniauth(request.env["omniauth.auth"])
+    logger.info  request.env["omniauth.auth"]
+
+    sign_in_and_redirect @user
+  end
+
 end
