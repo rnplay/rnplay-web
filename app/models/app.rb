eb3bf27de4ee7549df2f5994d667db63fd011a62
Joshua Sierles <joshua@diluvia.net>
2015-09-17 15:29:17 -0500

    use real packagers in staging

--- app/models/app.rb
+++ app/models/app.rb
@@ -21,8 +21,6 @@ class App < ActiveRecord::Base
   def bundle_url
     if Rails.env.development?
       "http://#{ENV['NGROK_SUBDOMAIN']}.ngrok.io#{bundle_path}"
-    elsif Rails.env.staging?
-      "https://packager-staging.rnplay.org#{bundle_path}"
     else
       "https://packager#{build.name.gsub(".", "").gsub("-", "")}.rnplay.org#{bundle_path}"
     end
