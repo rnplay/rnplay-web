080e8835af97ac3e2b367b9903b6d9451c779027
Joshua Sierles <joshua@diluvia.net>
2017-03-13 18:59:23 +0530

    announce rnplay.org shutdown, disable signups

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   acts_as_token_authenticatable
 
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable,
          :recoverable, :rememberable, :trackable,
          :omniauthable, :omniauth_providers => [:github]
 
